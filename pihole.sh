812e5af1904922f060ed754bc29cf68e1f2bd4c0
N2ITN <zestela@gmail.com>
2016-12-10 06:18:45 +0000

    Sync with current RPi dev branch

---
+++ pihole.sh
@@ -0,0 +1,4 @@
+IMAGE='diginc/pi-hole:arm'
+NIC='wlan0'
+IP=$(ip addr show $NIC | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+docker run  -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --name pihole -d $IMAGE
