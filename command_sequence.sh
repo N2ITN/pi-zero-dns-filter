6fc08d8ab0460fb87335db290d0c1a2006a21a7d
Zachary A Estela <zestela@gmail.com>
2016-12-12 02:02:32 -0800

    cmd updates

--- command_sequence.sh
+++ command_sequence.sh
@@ -38,6 +38,7 @@ sudo mv rc.local /etc/rc.local
 # Serve portainer container viewer from localhost if SSH session is started
 echo "docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock --name furiosa portainer/portainer:arm > error_catcher.s 2> /dev/null" >> ~/.profile 
 echo "docker start furiosa" >> ~/.profile
+echo "cd zero" >> ~/.profile
 
 
 # In case of git
