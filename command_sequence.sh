ffe5f209a4c172a87ab50c438cc3e59fddc95cf4
Zachary A Estela <zestela@gmail.com>
2016-12-11 19:00:03 -0800

    update docker image tag

--- command_sequence.sh
+++ command_sequence.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/N2ITN/pi-zero-accesspoint-adblocker.git zer0
-
+cd zer0
+bash pihole.sh
--- pihole.sh
+++ pihole.sh
@@ -1,4 +1,4 @@
-IMAGE='gojira00/pi-hole-2016'
+IMAGE='gojira00/pi-hole-2016:arm'
 NIC='wlan0'
 IP=$(ip addr show $NIC | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 docker run  -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --name pihole -d $IMAGE
