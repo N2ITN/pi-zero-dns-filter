891fc9be3e9fb1dba15928e22f7dcf8f25948247
Zachary A Estela <zestela@gmail.com>
2016-12-16 22:56:22 -0800

    update error handing, fatal and keyboard

--- web_AP.py
+++ web_AP.py
@@ -4,36 +4,31 @@ from time import sleep
 
 
 
-try:
-    ap = run_serial(wireless_AP)
-    sleep(7)
-    env = run_serial(envConf)    
-    fdns = run_serial(fakeDNS)
-    ws = run_serial(webServer)
 
-    def run_serial(commandList):
-        if len(commandList) == 1:
-            command = commandList[0]
-            print (command)
-        else: 
-            command = "; ".join(commandList)
+def run_serial(commandList):
+    if len(commandList) == 1:
+        command = commandList[0]
         print (command)
+    else: 
+        command = "; ".join(commandList)
+    print (command)
+    try:
+        subprocess.Popen(command,shell=True)
+    except Exception as e:
+        print (e)
+    print ()
+
+def terminus():
+    print ('terminating processes')    
+    for x in [ap, fnds,ws]:
         try:
-            subprocess.Popen(command,shell=True)
+            x.terminate()
         except Exception as e:
             print (e)
-        print ()
+    run_serial(restoreConf)
+    run_serial(envReset)
 
-    def terminus():
-        print ('terminating processes')    
-        for x in [ap, fnds,ws]:
-            try:
-                x.terminate()
-            except Exception as e:
-                print (e)
-        run_serial(restoreConf)
-        run_serial(envReset)
-
+try:
     wireless_AP = ["sudo create_ap -n wlan0 zer0 adzapper" ]
     envConf = ["export WLAN_ADDR=`ifconfig wlan0 | grep 'inet addr' | awk '{print $2}' | sed -e 's/:/\\n/' | grep 192`",
     'touch ~/zer0/resolv.conf && echo "nameserver $WLAN_ADDR" >> ~/zer0/resolv.conf && chmod 644 ~/zer0/resolv.conf',
@@ -45,12 +40,21 @@ try:
     envReset = ['touch ~/zer0/resolv.conf && chmod 644 ~/zer0/resolv.conf', 'sudo mv resolv.conf /etc/resolv.conf', 
     'touch ~/zer0/dnsmasq.hosts && chmod 644 ~/zer0/dnsmasq.hosts','sudo mv dnsmasq.hosts /etc/dnsmasq.hosts']
 
+    ap = run_serial(wireless_AP)
+    sleep(7)
+    env = run_serial(envConf)    
+    fdns = run_serial(fakeDNS)
+    ws = run_serial(webServer)
 
-except KeyboardInterrupt:
-    print ("KeyboardInterrupt")
-    terminus()
-    print (ws), print (type(ws))
-
+except Exception as e:
+    if isinstance(e, KeyboardInterrupt):
+        print ("KeyboardInterrupt")
+        terminus()
+        print (ws), print (type(ws))
+    else: 
+        "Error"
+        print (e)
+        terminus()
 
 
 
