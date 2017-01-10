8042613a4205d606a14c6b65a3c342c7802bd9df
Zachary A Estela <zestela@gmail.com>
2017-01-10 12:06:59 -0800

    persist script

---
+++ pihole_persist.sh
@@ -0,0 +1,18 @@
+sudo mkdir mnt
+sudo mkdir mnt/etc/
+sudo mkdir mnt/log/
+sudo mkdir mnt/html
+sudo chmod 755 mnt/html
+sudo touch mnt/log/pihole.log
+sudo chmod 755  mnt/log/pihole.log
+docker stop pihole
+docker rm pihole
+IMAGE='diginc/pi-hole:arm'
+NIC='wlan0'
+IP=$(ip addr show $NIC | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+docker run  --restart=always  -v ~/mnt/log/pihole.log:/var/log/pihole.log -v ~/mnt/etc/:/etc/pihole/ -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP=$
+
+# Recommended auto ad list updates & log rotation:
+wget -O- https://raw.githubusercontent.com/diginc/docker-pi-hole/master/docker-pi-hole.cron | sudo tee /etc/cron.d/docker-pi-hole
+
+
