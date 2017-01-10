15844c6753e6390530b2dabea7068f566d546288
Zachary A Estela <zestela@gmail.com>
2017-01-10 13:58:35 -0800

    tweaks

--- webAP/webserver.py
+++ webAP/webserver.py
@@ -28,7 +28,8 @@ class myHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', mimetype)
             self.end_headers()
-            self.wfile.write("<p>You accessed path: %s</p>" % f.read())
+            
+            self.wfile.write("<p>You accessed path: %s</p>" % bytes(f.read(), "utf-8") )
             f.close()
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
