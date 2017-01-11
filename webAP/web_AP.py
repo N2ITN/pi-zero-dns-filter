0ad1d1a145bcdfb2212a7faa3a9c908da6ccb348
Zachary A Estela <zestela@gmail.com>
2017-01-10 18:00:10 -0800

    fixfixfix

--- webAP/web_AP.py
+++ webAP/web_AP.py
@@ -1,29 +1,11 @@
 import subprocess
-from time import sleep
-import shlex
-
-
-def run_serial(commandList):
-    if len(commandList) == 1:
-        command = commandList[0]
-        print(command)
-    else:
-        command = "; ".join(commandList)
-    print(command)
-    try:
-        subprocess.Popen(command, shell=True)
-    except Exception as e:
-        print(e)
-    print()
-
 
 ssid = "iwconfig wlan0 | grep ESSID | awk -F: '{print $2}'"
 if len(str(subprocess.check_output(ssid, shell=True)).split('"')[1]) > 0:
-    subprocess.call("cd ~ && bash ~/pihole_persist.sh")
+    subprocess.call("cd ~ && bash ~/pihole_persist.sh", shell=True)
 else:
     os.chdir('~/')
     wireless_AP = ["sudo create_ap -n wlan0 zer0 adzapper"]
     webServer = ['sudo python3 webserver.py']
-    actionItems = [run_serial(wireless_AP), run_serial(webServer)]
 
-
+    subprocess.Popen('; '.join([wireless_AP, webServer]), shell=True)
