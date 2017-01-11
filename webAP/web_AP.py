7ca557695b01eabb9757b4694ce6258c0ddcd7e3
Zachary A Estela <zestela@gmail.com>
2017-01-11 13:25:06 -0800

    error fix

--- webAP/web_AP.py
+++ webAP/web_AP.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 ssid_get = "iwconfig wlan0 | grep ESSID | awk -F: '{print $2}'"
-ssid_bash = str(subprocess.check_output(ssid, shell=False))
+ssid_bash = str(subprocess.check_output(ssid_get, shell=False))
 ssid = ssid_bash.split('"')[1]
 if len(ssid) > 0:
     if not os.path.exists("/home/pirate/mnt"):
