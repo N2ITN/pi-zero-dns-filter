a6c6f652c31c70abdb1ebe02c96964fd2f5d9746
Zachary A Estela <zestela@gmail.com>
2017-01-10 18:15:18 -0800

    add mkdirs case

---
+++ mk_dirs.sh
@@ -0,0 +1,8 @@
+cd /home/pirate/
+sudo mkdir mnt
+sudo mkdir mnt/etc/
+sudo mkdir mnt/log/
+sudo mkdir mnt/html
+sudo chmod 755 mnt/html
+sudo touch mnt/log/pihole.log
+sudo chmod 755  mnt/log/pihole.log
--- webAP/web_AP.py
+++ webAP/web_AP.py
@@ -1,13 +1,15 @@
 import subprocess
+import os
 
 ssid = "iwconfig wlan0 | grep ESSID | awk -F: '{print $2}'"
 ssid_bash = str(subprocess.check_output(ssid, shell=True))
 print(ssid_bash)
 if len(ssid_bash.split('"')[1]) > 0:
+    if not os.path.exists("~/mnt"):
+        subprocess.call("mk_dirs.sh", shell=True)
     subprocess.call("bash ~/pihole_persist.sh", shell=True)
 else:
     os.chdir('~/')
     wireless_AP = ["sudo create_ap -n wlan0 zer0 adzapper"]
     webServer = ['sudo python3 webserver.py']
-
     subprocess.Popen('; '.join([wireless_AP, webServer]), shell=True)
