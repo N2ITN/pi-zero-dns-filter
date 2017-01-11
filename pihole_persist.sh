00a13a5d611bab162ee26a571df248b7beb87d37
Zachary A Estela <zestela@gmail.com>
2017-01-10 18:11:25 -0800

    fixes to docker

--- pihole_persist.sh
+++ pihole_persist.sh
@@ -1,10 +1,10 @@
-sudo mkdir mnt
-sudo mkdir mnt/etc/
-sudo mkdir mnt/log/
-sudo mkdir mnt/html
-sudo chmod 755 mnt/html
-sudo touch mnt/log/pihole.log
-sudo chmod 755  mnt/log/pihole.log
+# sudo mkdir mnt
+# sudo mkdir mnt/etc/
+# sudo mkdir mnt/log/
+# sudo mkdir mnt/html
+# sudo chmod 755 mnt/html
+# sudo touch mnt/log/pihole.log
+# sudo chmod 755  mnt/log/pihole.log
 docker stop pihole
 docker rm pihole
 IMAGE='diginc/pi-hole:arm'
