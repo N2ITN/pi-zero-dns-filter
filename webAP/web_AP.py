fac4aec97c5b76a2060b7035129a5a2445e80e9d
Zachary A Estela <zestela@gmail.com>
2017-01-10 18:06:08 -0800

    no need for sudo

--- webAP/web_AP.py
+++ webAP/web_AP.py
@@ -1,8 +1,10 @@
 import subprocess
 
 ssid = "iwconfig wlan0 | grep ESSID | awk -F: '{print $2}'"
-if len(str(subprocess.check_output(ssid, shell=True)).split('"')[1]) > 0:
-    subprocess.call("su pirate && cd ~ && bash ~/pihole_persist.sh", shell=True)
+ssid_bash = str(subprocess.check_output(ssid, shell=True)
+print ssid_bash
+if len(ssid_bash).split('"')[1]) > 0:
+    subprocess.call("bash ~/pihole_persist.sh", shell=True)
 else:
     os.chdir('~/')
     wireless_AP = ["sudo create_ap -n wlan0 zer0 adzapper"]
