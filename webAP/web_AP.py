6d4add0c9dd5407efcf8bc4b7662f84037128305
Zachary A Estela <zestela@gmail.com>
2017-01-11 18:58:59 -0800

    fix

--- webAP/web_AP.py
+++ webAP/web_AP.py
@@ -13,5 +13,5 @@ try:
 except IndexError:
     wireless_AP = "sudo create_ap -n wlan0 zer0 adzapper"
     webServer = "sudo python3 /home/pirate/pi-zero-master/webAP/webserver.py"
-    subprocess.Popen(wireless_AP), shell=True)
+    subprocess.Popen(wireless_AP, shell=True)
     subprocess.Popen(webServer), shell=True)
