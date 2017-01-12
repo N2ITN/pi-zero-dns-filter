c5795cfccca81def51f782dbe1ee93c9329f4d4c
Zachary A Estela <zestela@gmail.com>
2017-01-11 17:07:44 -0800

    update web_AP logic

--- webAP/web_AP.py
+++ webAP/web_AP.py
@@ -3,13 +3,15 @@ import os
 
 ssid_get = "iwconfig wlan0 | grep ESSID | awk -F: '{print $2}'"
 ssid_bash = str(subprocess.check_output(ssid_get, shell=True))
-ssid = ssid_bash.split('"')[1]
-if len(ssid) > 0:
+
+try:
+    ssid = ssid_bash.split('"')[1]
     if not os.path.exists("/home/pirate/mnt"):
         subprocess.call("mk_dirs.sh", shell=True)
     subprocess.Popen("bash ~/pi-zero-master/pihole_persist.sh", shell=True)
-else:
+
+except IndexError:
     os.chdir('~/')
-    wireless_AP = ["sudo create_ap -n wlan0 zer0 adzapper"]
+    wireless_AP = ["sudo ifdown wlan0; sudo create_ap -n wlan0 zer0 adzapper"]
     webServer = ['sudo python3 webserver.py']
-    subprocess.Popen('; '.join([wireless_AP, webServer]), shell=True)
+    subprocess.Popen(''.join([wireless_AP, webServer]), shell=True)
