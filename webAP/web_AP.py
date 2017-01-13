00023a0d93d15b0a0ab2728387ac6d7f18e6700a
Zachary A Estela <zestela@gmail.com>
2017-01-12 19:18:35 -0800

    netstat -plnt before decision fork

--- webAP/web_AP.py
+++ webAP/web_AP.py
@@ -4,13 +4,15 @@ import time
 
 ssid_get = "iwconfig wlan0 | grep ESSID | awk -F: '{print $2}'"
 ssid_bash = str(subprocess.check_output(ssid_get, shell=True))
+check_ports = "sudo netstat -plnt"
+print(subprocess.check_call(check_ports, shell=True))
 
 try:
     ssid = ssid_bash.split('"')[1]
     print('Connected to', ssid)
     if not os.path.exists("/home/pirate/mnt"):
         subprocess.call("mk_dirs.sh", shell=True)
-    subprocess.Popen("bash ~/pi-zero-master/pihole_persist.sh", shell=True)
+    subprocess.Popen("bash /home/pirate/pi-zero-master/pihole_persist.sh", shell=True)
 
 except IndexError as e:
     print(e)
@@ -18,7 +20,5 @@ except IndexError as e:
     wireless_AP = "sudo create_ap --no-virt -n wlan0 zer0 adzapper && sudo service avahi-daemon restart"
     subprocess.Popen(wireless_AP, shell=True)
     time.sleep(10)
-    check_ports = "sudo netstat -plnt"
-    print(subprocess.check_call(check_ports, shell=True))
     webserver = "sudo python3 /home/pirate/pi-zero-master/webAP/webserver.py &> ~/webServerlog"
     subprocess.Popen(webserver, shell=True)
