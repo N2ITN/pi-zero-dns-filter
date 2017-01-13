80de98b73a20b39d703dddd3ee6e4d227d4bc4df
Zach Estela <z@aracel.io>
2017-01-13 12:15:15 -0800

    add /etc/init.d service file

---
+++ piholeAP.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo “Starting piholeAP service”
+echo date
+export IP=$(ifconfig wlan0 | grep 'inet addr' | awk '{print $2}' | sed -e 's/:/\n/' | grep 192)
+cd ~/pi-zero-master/webAP/ && sudo python3 web_AP.py
--- pihole_persist.sh
+++ pihole_persist.sh
@@ -3,7 +3,9 @@ docker rm pihole
 IMAGE='diginc/pi-hole:arm'
 NIC='wlan0'
 IP=$(ip addr show $NIC | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-docker run  -v ~/mnt/log/pihole.log:/var/log/pihole.log -v ~/mnt/etc/:/etc/pihole/ -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --name pihole -d $IMAGE
+echo $IP
+sudo service dnsmasq stop
+docker run -v /home/pirate/mnt/log/pihole.log:/var/log/pihole.log -v /home/pirate/mnt/etc/:/etc/pihole/ -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --name pihole -d $IMAGE
 
 # Recommended auto ad list updates & log rotation:
 wget -O- https://raw.githubusercontent.com/diginc/docker-pi-hole/master/docker-pi-hole.cron | sudo tee /etc/cron.d/docker-pi-hole
