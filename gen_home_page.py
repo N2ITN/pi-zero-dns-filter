ff3697d1effccbb3ca187e31c03d4aff324fa082
Zachary A Estela <zestela@gmail.com>
2017-01-14 02:25:14 -0800

    semantics

--- gen_home_page.py
+++ gen_home_page.py
@@ -3,7 +3,7 @@ import subprocess
 with open('home.html', 'w') as page:
     ip = subprocess.check_output("echo $IP", shell=True).decode('utf-8')
     router = 'http://' + subprocess.check_output("echo $ROUTER", shell=True).decode('utf-8')
-    routerlink = ''.join(['<a class="button button-outline" href=', router,'> Your Router </a>'])
+    routerlink = ''.join(['<a class="button button-outline" href=', router,'> Your Router Settings</a>'])
     dnslabel = ' '.join(['<label>Set your DNS to: </label>', '<t>', ip, '</t>'])
     page.write('''
 
