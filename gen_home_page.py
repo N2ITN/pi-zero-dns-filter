f7dffd2303d4e0dea7f5b677baa5405f89c1ae4a
Zachary A Estela <zestela@gmail.com>
2017-01-14 00:17:12 -0800

    add css + tags

--- gen_home_page.py
+++ gen_home_page.py
@@ -1,15 +1,22 @@
 import subprocess
 
 with open('home.html', 'w') as page:
-    ip =  subprocess.check_output(
-        "echo $IP", shell=True).decode('utf-8')
-    router =  subprocess.check_output(
-        "echo $ROUTER", shell=True).decode('utf-8')
+    ip = subprocess.check_output("echo $IP", shell=True).decode('utf-8')
+    router = subprocess.check_output("echo $ROUTER", shell=True).decode('utf-8')
+    page.write('''
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
 
+
+
+<!-- Milligram CSS minified -->
+<link rel="stylesheet" href="milligram/dist/milligram.min.css">
+
+<!-- You should properly set the path from the main file. -->''')
     page.write("\n".join([
-        '<html>', '<body>', '<label>Your router: </label>',  '<t>',
-        '<a href="','http://', router, '">', router, '</a>', '<br>','</t>', '<br>',
-        '<label>Set your DNS to: </label>', '<t>', ip, '</t>', '<br>',
-        '<br>', '<a href="', "/admin", '">', 'Pi-Hole dashboard ', '</a>',
-        '</body>', '</html>'
+        '<html>', '<body>', '<label>Your router: </label>', '<t>', '<a href="',
+        'http://', router, '">', router, '</a>', '<br>', '</t>', '<br>',
+        '<label>Set your DNS to: </label>', '<t>', ip, '</t>', '<br>', '<br>',
+        '<a href="', "/admin", '">', 'Pi-Hole dashboard ', '</a>', '</body>',
+        '</html>'
     ]))
