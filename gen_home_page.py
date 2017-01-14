b3ae08504a6f0a1732467d3c4f39e1c621e3d604
Zachary A Estela <zestela@gmail.com>
2017-01-13 22:41:50 -0800

    add router deteection + html gen

--- gen_home_page.py
+++ gen_home_page.py
@@ -1,15 +1,13 @@
-
 import subprocess
 
+
 with open('home.html', 'w') as page:
-    head = """<html>
-    <body>"""
+    ip = str(subprocess.check_output("echo $IP", shell=True))
+    router = str(subprocess.check_output("echo $ROUTER", shell=True))
+    router = str(subprocess.check_output("""netstat -nr | awk '$1 == "0.0.0.0"{print$2}'""")
 
-    middle = subprocess.check_output("echo $IP",shell=True)
-
-    end = """</body>
-    </html>"""
-    page.write(head)
-    page.write(middle)
-    page.write(end)
-
+    page.write("\n".join([
+        '<html>', '<body>', '<label>Your network: </label>', '<t>', ip, '</t>', '<br>',
+        '<label>Your router: </label>', '<t>', router, '</t>', '</body>',
+        '</html>'
+    ]))
--- pihole_persist.sh
+++ pihole_persist.sh
@@ -3,6 +3,8 @@ docker rm pihole
 IMAGE='diginc/pi-hole:arm'
 NIC='wlan0'
 IP=$(ip addr show $NIC | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+ROUTER=$(netstat -nr | awk '$1 == "0.0.0.0"{print$2}')
+python3 gen_home_page.py
 echo $IP
 sudo service dnsmasq stop
 docker run -v /home/pirate/mnt/log/pihole.log:/var/log/pihole.log -v /home/pirate/mnt/etc/:/etc/pihole/ -v ~/mnt/html/pihole:/var/www/html/pihole -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --name pihole -d $IMAGE
