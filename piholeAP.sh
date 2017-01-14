0364ad5f702bcb408f9e7026ba354bce0c78e48e
Zach Estela <z@aracel.io>
2017-01-14 06:29:11 -0800

    host side fixes

--- piholeAP.sh
+++ piholeAP.sh
@@ -1,4 +1,5 @@
 echo “Starting piholeAP service”
 echo date
 export IP=$(ifconfig wlan0 | grep 'inet addr' | awk '{print $2}' | sed -e 's/:/\n/' | grep 192)
+sudo ifconfig wlan0 up
 cd /home/pirate//pi-zero-master/webAP/ && sudo python3 web_AP.py
