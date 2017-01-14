3295b6f31de5b46934ec9c047fa633d1919570a0
Zachary A Estela <zestela@gmail.com>
2017-01-13 23:27:03 -0800

    fix

--- gen_home_page.py
+++ gen_home_page.py
@@ -1,12 +1,15 @@
 import subprocess
 
 with open('home.html', 'w') as page:
-    ip = 'http://' + subprocess.check_output("echo $IP", shell=True).decode('utf-8')
-    router = 'http://' +  subprocess.check_output("echo $ROUTER", shell=True).decode('utf-8')
+    ip = 'http://' + subprocess.check_output(
+        "echo $IP", shell=True).decode('utf-8')
+    router = 'http://' + subprocess.check_output(
+        "echo $ROUTER", shell=True).decode('utf-8')
 
     page.write("\n".join([
         '<html>', '<body>', '<label>Your network: </label>', '<t>', '<a href="',
-        ip, '">', ip, '</a>', '</t>', '<br>', '<label>Your router: </label>',
-        '<t>', '<a href="', router, '">', router, '</a>', '</t>', '<br>',
-        '<a href="', "/admin", '">', 'Pi-Hole dashboard ', '</a>', '</body>', '</html>'
+        ip, '">', ip, '</a>', '</t>', '<br>',
+        '<label>Set your DNS to: </label>', '<t>', router, '</t>', '<br>',
+        '<a href="', "/admin", '">', 'Pi-Hole dashboard ', '</a>', '</body>',
+        '</html>'
     ]))
