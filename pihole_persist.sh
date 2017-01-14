094bb42750b69619f653beccd809918960ecd382
Zachary A Estela <zestela@gmail.com>
2017-01-13 19:03:08 -0800

    add html vol to pihole_persist.sh

--- pihole_persist.sh
+++ pihole_persist.sh
@@ -5,7 +5,7 @@ NIC='wlan0'
 IP=$(ip addr show $NIC | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 echo $IP
 sudo service dnsmasq stop
-docker run -v /home/pirate/mnt/log/pihole.log:/var/log/pihole.log -v /home/pirate/mnt/etc/:/etc/pihole/ -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --name pihole -d $IMAGE
+docker run -v /home/pirate/mnt/log/pihole.log:/var/log/pihole.log -v /home/pirate/mnt/etc/:/etc/pihole/ -v ~/mnt/html/pihole:/var/www/html/pihole -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --name pihole -d $IMAGE
 
 # Recommended auto ad list updates & log rotation:
 wget -O- https://raw.githubusercontent.com/diginc/docker-pi-hole/master/docker-pi-hole.cron | sudo tee /etc/cron.d/docker-pi-hole
