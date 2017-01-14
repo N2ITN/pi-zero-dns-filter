3b73ae235ca4d00abf0d3d955b31ade283f44b2c
Zachary A Estela <zestela@gmail.com>
2017-01-13 23:42:39 -0800

    fix html

--- gen_home_page.py
+++ gen_home_page.py
@@ -3,13 +3,13 @@ import subprocess
 with open('home.html', 'w') as page:
     ip = 'http://' + subprocess.check_output(
         "echo $IP", shell=True).decode('utf-8')
-    router = 'http://' + subprocess.check_output(
+    router =  subprocess.check_output(
         "echo $ROUTER", shell=True).decode('utf-8')
 
     page.write("\n".join([
-        '<html>', '<body>', '<label>Your network: </label>',  '<t>',
-        '<a href="', ip, '">', ip, '</a>', '<br>','</t>', '<br>',
-        '<label>Set your DNS to: </label>', '<t>', router, '</t>', '<br>',
+        '<html>', '<body>', '<label>Your router: </label>',  '<t>',
+        '<a href="','http://', router, '">', router, '</a>', '<br>','</t>', '<br>',
+        '<label>Set your DNS to: </label>', '<t>', ip, '</t>', '<br>',
         '<br>', '<a href="', "/admin", '">', 'Pi-Hole dashboard ', '</a>',
         '</body>', '</html>'
     ]))
