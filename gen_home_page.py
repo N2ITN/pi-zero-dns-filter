e451c2e4414c1ea83b3d6afc8d74e8699866fc3b
Zachary A Estela <zestela@gmail.com>
2017-01-13 22:48:08 -0800

    fix

--- gen_home_page.py
+++ gen_home_page.py
@@ -1,13 +1,11 @@
 import subprocess
 
-
 with open('home.html', 'w') as page:
     ip = str(subprocess.check_output("echo $IP", shell=True))
     router = str(subprocess.check_output("echo $ROUTER", shell=True))
-    router = str(subprocess.check_output("""netstat -nr | awk '$1 == "0.0.0.0"{print$2}'""")
 
     page.write("\n".join([
-        '<html>', '<body>', '<label>Your network: </label>', '<t>', ip, '</t>', '<br>',
-        '<label>Your router: </label>', '<t>', router, '</t>', '</body>',
-        '</html>'
+        '<html>', '<body>', '<label>Your network: </label>', '<t>', ip, '</t>',
+        '<br>', '<label>Your route r: </label>', '<t>', router, '</t>',
+        '</body>', '</html>'
     ]))
