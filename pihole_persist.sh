b4e7b3b216655eda565f40ce6ea4e64e20637e37
Zachary A Estela <zestela@gmail.com>
2017-01-13 23:01:25 -0800

    fix

--- pihole_persist.sh
+++ pihole_persist.sh
@@ -8,7 +8,7 @@ python3 gen_home_page.py
 sudo cp home.html /home/pirate/mnt/html/index.html
 echo $IP
 sudo service dnsmasq stop
-docker run -v /home/pirate/mnt/log/pihole.log:/var/log/pihole.log -v /home/pirate/mnt/etc/:/etc/pihole/ -v ~/mnt/html/pihole:/var/www/html/pihole -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --name pihole -d $IMAGE
+docker run -v /home/pirate/mnt/log/pihole.log:/var/log/pihole.log -v /home/pirate/mnt/etc/:/etc/pihole/ -v /home/pirate/mnt/html/:/var/www/html/ -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --name pihole -d $IMAGE
 
 # Recommended auto ad list updates & log rotation:
 wget -O- https://raw.githubusercontent.com/diginc/docker-pi-hole/master/docker-pi-hole.cron | sudo tee /etc/cron.d/docker-pi-hole
