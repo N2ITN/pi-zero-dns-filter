c4fc09b346a89728f590521f882bb7eb2152cad2
Zachary A Estela <zestela@gmail.com>
2017-01-13 16:18:46 -0800

    html fixes

--- webAP/gen_drop_down.py
+++ webAP/gen_drop_down.py
@@ -15,7 +15,7 @@ with open("app.html", 'r') as original:
     with open('app_new.html', 'w') as new:
         for line in original.readlines():
             new.write(line)
-            if '<input type="text" name="network" />' in line:
+            if '<select name="network">' in line:
                 for x in localNetworks:
                     new.write(x)
                     new.write('\n')
