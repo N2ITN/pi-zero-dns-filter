88f7d15cc003c6eea5b2184d43dbe960e85cdfc6
Zachary A Estela <zestela@gmail.com>
2017-01-21 23:10:45 -0800

    piholeAP.sh: line 3: ifconfig: command not found SIOCSIFFLAGS: Operation not permitted

--- piholeAP.sh
+++ piholeAP.sh
@@ -1,7 +1,7 @@
 echo “Starting piholeAP service”
 echo date
 export IP=$(ifconfig wlan0 | grep 'inet addr' | awk '{print $2}' | sed -e 's/:/\n/' | grep 192)
-/sbin/ifconfig wlan0 down
-/sbin/ifconfig wlan0 up
+sudo /sbin/ifconfig wlan0 down
+sudo /sbin/ifconfig wlan0 up
 sleep 5
 cd /home/pirate//pi-zero-master/webAP/ && sudo python3 web_AP.py
